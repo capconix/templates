9b6020324e791da5ca8f0cefe4b2988cb34c151f
Daniel CapConiX <daniel@capconix.com>
2019-12-27 01:45:31 -0500

    Added header file

---
+++ header.py
@@ -0,0 +1,19 @@
+###############################################################################
+#                                                                             #
+#   CapConiX LLC                                                              #
+#                                                                             #
+#   Project : [Project Name]                                                  #
+#   Creation Date : [YYYY-MM-DD]                                              #
+#   Original Developer : [NAME]                                               #
+#                                                                             #
+#   Title : [Title of document]                                               #
+#   File Name : [File Name]                                                   #
+#                                                                             #
+#   Purpose / Description :                                                   #
+#   [TEXT GOES HERE!]                                                         #
+#                                                                             #
+#   Change Log :                                                              #
+#   [YYYY-MM-DD]    [Name]                                                    #
+#   [Change Description]                                                      #
+#                                                                             #
+###############################################################################
